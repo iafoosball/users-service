ff8c537c3fbe7a88b205dd52f686032110398f69
jonas27 <jonas.burster@gmail.com>
2018-10-15 10:35:41 +0200

    COmmit changes to users for aleks to start working

--- users/users_test.go
+++ users/users_test.go
@@ -1,11 +1,22 @@
 package users
 
 import (
-	//"github.com/iafoosball/users-service/restapi/operations"
+	"bytes"
+	"encoding/json"
+	"github.com/iafoosball/users-service/models"
+	"log"
+	"net/http"
 	"testing"
 )
 
 func TestCreateUser(t *testing.T) {
-	//var user operations.PostUsersHandlerFunc
-	//user(users.CreateUser())
+	u := models.User{
+		Username: "Username",
+		Lastname: "Lastname",
+	}
+	json, _ := json.Marshal(&u)
+	if resp, err := http.Post("http://localhost:9001/users/createUser", "application/json", bytes.NewReader(json)); err != nil || http.StatusOK != resp.StatusCode {
+		log.Fatal("User could not be created")
+	}
+
 }
